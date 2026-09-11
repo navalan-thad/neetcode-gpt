c3ca49cc44f9738e8660d58d02b7329f881daa8e
Navalan Thad <85454400+navalan-thad@users.noreply.github.com>
2026-09-11 16:15:45 -0400

    Update Training loop (Training 1/4)

--- foundations/training_loop.py
+++ foundations/training_loop.py
@@ -7,14 +7,14 @@ class Solution:
     def train(self, X: NDArray[np.float64], y: NDArray[np.float64], epochs: int, lr: float) -> Tuple[NDArray[np.float64], float]:
         # X: (n_samples, n_features)
         # y: (n_samples,) targets
-        # epochs: number of training iterations
-        # lr: learning rate
-        #
-        # Model: y_hat = X @ w + b
-        # Loss: MSE = (1/n) * sum((y_hat - y)^2)
-        # Initialize w = zeros, b = 0
-        # return (np.round(w, 5), round(b, 5))
-        
+
+        """
+        since y_hat = X@w + b, dydw = X, dydb = 1
+        since L = 1/n * (y_hat - y)^2, dLdy = 2/n * (y_hat-y)
+        thus, dLdw = dLdy * dydw = 2/n * (y_hat-y) * X
+        and dLdb = dLdy * dydb = 2/n * (y_hat-y) * 1
+        """
+    
         w = np.zeros((X.shape[1],))
         b = 0
         n = X.shape[0]
@@ -22,11 +22,12 @@ class Solution:
         for _ in range(epochs):
             y_hat = X @ w + b
             loss = np.mean((y - y_hat)**2)
-            dLdW = (2/n) * (X.T @ (y_hat - y))
+
+            dLdw = (2/n) * (X.T @ (y_hat - y))
             dLdb = (2/n) * np.sum(y_hat - y)
 
-            w -= dLdW*lr
+            w -= dLdw*lr
             b -= dLdb*lr
-
+        
         return (np.round(w, 5), round(b, 5))
 
